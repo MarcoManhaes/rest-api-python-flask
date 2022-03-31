08581418ee149d20835562960394af75c5dafc0f
Marco Manhães <marco.manhaes@gmail.com>
2022-03-30 22:22:22 -0300

    adicionado relacionamento entre tabelas com alchemy, delete em cascata

--- models/hotel.py
+++ models/hotel.py
@@ -9,13 +9,16 @@ class HotelModel(banco.Model):
     estrelas = banco.Column(banco.Float(precision=1))
     diaria = banco.Column(banco.Float(precision=2))
     cidade = banco.Column(banco.String(40))
+    site = banco.relationship('SiteModel')
+    site_id = banco.Column(banco.Integer, banco.ForeignKey('sites.site_id'))
 
-    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
+    def __init__(self, hotel_id, nome, estrelas, diaria, cidade, site_id):
         self.hotel_id = hotel_id
         self.nome = nome
         self.estrelas = estrelas
         self.diaria = diaria
         self.cidade = cidade
+        self.site_id = site_id
 
     # retorna modelo json do objeto
     def json(self):
@@ -24,7 +27,9 @@ class HotelModel(banco.Model):
             'nome': self.nome,
             'estrelas': self.estrelas,
             'diaria': self.diaria,
-            'cidade': self.cidade
+            'cidade': self.cidade,
+            'site_id': self.site_id,
+            # 'site': self.site.json()
         }
 
     @classmethod
---
+++ resources/site.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource
+from models.site import SiteModel
+
+
+class Sites(Resource):
+    def get(self):
+        return {'sites': [site.json() for site in SiteModel.query.all()]}
+
+
+class Site(Resource):
+    def get(self, url):
+        site = SiteModel.find_site(url)
+        if site:
+            return site.json()
+        return{'message': 'Site not found.'}, 404
+
+    def post(self, url):
+        if SiteModel.find_site(url):
+            return{'message': "The site '{}' already exists.".format(url)}, 400
+        site = SiteModel(url)
+        try:
+            site.save_site()
+        except:
+            return {'message': 'An internal error ocorred trying to create a new site.'}, 500
+        return site.json()
+
+    def delete(self, url):
+        site = SiteModel.find_site(url)
+        if site:
+            site.delete_site()
+            return {'message': 'Site deleted.'}
+        return {'message': 'Site not found.'}, 404
